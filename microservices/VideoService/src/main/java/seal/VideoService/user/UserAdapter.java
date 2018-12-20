996190adb20b7161b2e075b49e8f1443db40a256
Unknown <patcharapol.top@mail.kmutt.ac.th>
2018-12-20 23:44:43 +0700

    [ADD] ทำ adapter user ให้ใช้ร่วมกับ comment

---
+++ microservices/VideoService/src/main/java/seal/VideoService/user/UserAdapter.java
@@ -0,0 +1,14 @@
+package seal.VideoService.user;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class UserAdapter {
+  public User getUserDetail(long userId) {
+    RestTemplate restTemplate = new RestTemplate();
+    String url = "http://localhost:3006/user/" + userId;
+    User user = restTemplate.getForObject(url, User.class);
+    return user;
+  }
+}
