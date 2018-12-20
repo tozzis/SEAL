ed97963043c499f87bf5d554c8c697190ae6642a
Unknown <patcharapol.top@mail.kmutt.ac.th>
2018-12-20 23:45:58 +0700

    [EDIT] แก้ไข comment ให้มีข้อมูลของ user

--- microservices/VideoService/src/main/java/seal/VideoService/comment/Comment.java
+++ microservices/VideoService/src/main/java/seal/VideoService/comment/Comment.java
@@ -1,19 +1,30 @@
 package seal.VideoService.comment;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import seal.VideoService.user.User;
 
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 
 @Entity
 @Table(name = "comments")
@@ -23,36 +34,48 @@ public class Comment implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "user_id")
-    private long userId;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+    private User user;
 
     @Column(name = "video_id")
-    @NotBlank
+    @NotNull
     private String videoId;
 
-    @NotBlank
+    @Column(name = "comment")
+    @NotNull
     private String comment;
     
     @Column(name = "created_at")
     @Temporal(TemporalType.TIMESTAMP)
     @CreatedDate
+    @NotNull
     private Date createdAt;
     
     @Column(name = "updated_at")
     @Temporal(TemporalType.TIMESTAMP)
     @LastModifiedDate
+    @NotNull
     private Date updatedAt;
 
     public Comment() {
     }
 
-    public Comment(long id, long userId, String videoId, String comment, Date createdAt, Date updatedAt) {
+    public Comment(long id, User user, String videoId, String comment, Date createdAt, Date updatedAt) {
         this.id = id;
-        this.userId = userId;
+        this.user = user;
         this.videoId = videoId;
         this.comment = comment;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
+    }
+
+    public Comment(User user, String videoId, String comment) {
+        this.user = user;
+        this.videoId = videoId;
+        this.comment = comment;
     }
 
     public long getId() {
@@ -63,12 +86,12 @@ public class Comment implements Serializable {
         this.id = id;
     }
 
-    public long getUserId() {
-        return userId;
+    public User getUser() {
+        return user;
     }
 
-    public void setUserId(long userId) {
-        this.userId = userId;
+    public void setUser(User user) {
+        this.user = user;
     }
 
     public String getVideoId() {
@@ -102,4 +125,5 @@ public class Comment implements Serializable {
     public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
     }
+
 }
